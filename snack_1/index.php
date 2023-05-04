02828534b0d9e03fd0645b5e5de19c985898adbe
Dan97O <dan97-oddo@live.it>
2023-05-04 12:10:01 +0200

    initial commit array matches

---
+++ snack_1/index.php
@@ -0,0 +1,29 @@
+<?php  
+/* 
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+
+Olimpia Milano - Cantù | 55-60
+*/
+
+$matches = [
+  ["Olimpia Milano", "Cantù", 55, 60],
+  ["Virtus Bologna", "Bologna", 70, 70],
+  ["Firenze", "Torino", 62, 75],
+  ["Virtus Roma", "Venezia", 68, 83]
+];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
